a1fc3c52472d0a3518d903c2268e602646824b86
LIU <liuyf211@gmail.com>
2018-01-24 23:15:05 -0600

    no message

--- 1/Problem2.R
+++ 1/Problem2.R
@@ -1,14 +1,23 @@
-n = c(10, 30, 50, 100)
-p = c(0.10, 0.25, 0.50, 0.75, 0.90)
-count  = 1
-par(mfrow=c(2,2))
-for( i in n){
-  for(j in p) {
-    p_temp = c()
-    p_temp=replicate(500, mean(rbinom(i, 1, j)))
-    
-    qqnorm(y=p_temp, main=paste("title: n = ", i, ", p = ", j))
-    qqline(p_temp)
-    count = count + 1
+########################
+# n: array of sample size
+# p: array of probability
+#######################
+P2 = function(n, p){
+  # set graphical parameters
+  par(mfrow=c(2,2))
+  
+  for( i in n){
+    for(j in p) {
+      # simulate 500 for each (n, p)
+      p_temp=replicate(500, mean(rbinom(i, 1, j)))
+      # draw Q-Q plot
+      qqnorm(y=p_temp, main=paste("title: n = ", i, ", p = ", j))
+      qqline(p_temp)
+    }
   }
 }
+
+# call the function
+n = c(10, 30, 50, 100)
+p = c(0.10, 0.25, 0.50, 0.75, 0.90)
+P2(n, p)
